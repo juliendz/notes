f53eeb3afb73b9a9310bbe588071a0b71d6f7b18
Julien Dcruz <juliendcruz@gmail.com>
2017-11-18 18:00:08 +0530

    Add list for notes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include "notewindow.h"
 #include <QApplication>
+#include <QList>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    NoteWindow w;
-    w.show();
 
-    return a.exec();
+void createNoteWindow(QList<QWidget*> * NOTES){
+    NoteWindow* w = new NoteWindow();
+    NOTES->append(w);
 }
+
+int main(int argc, char *argv[]) {
+
+    QApplication a(argc, argv);
+    QList<QWidget*> NOTES;
+
+    //Load notes from settings
+
+    if(NOTES.count() == 0){
+        createNoteWindow(&NOTES);
+    }
+
+    for(int i=0; i!= NOTES.count(); i++){
+        NOTES.at(i)->move(700, NOTES.at(i)->pos().y());
+        NOTES.at(i)->show();
+    }
+
+    int ret = a.exec();
+
+    //Clean up
+    qDeleteAll(NOTES.begin(), NOTES.end());
+    NOTES.clear();
+
+    return ret;
+}
+
+
